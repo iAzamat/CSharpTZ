7dcacdaa597a9516a25301e95f130ac541009abd
Azamat <isabirovazamati@gmail.com>
2023-09-27 15:31:28 +0500

    Initial commit

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ReadExcelFileApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            btnChoose = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            btnChoose.Location = new Point(11, 11);
+            btnChoose.Margin = new Padding(2);
+            btnChoose.Name = "btnChoose";
+            btnChoose.Size = new Size(82, 23);
+            btnChoose.TabIndex = 0;
+            btnChoose.Text = "Choose File";
+            btnChoose.UseVisualStyleBackColor = true;
+            btnChoose.Click += btnChoose_Click;
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(109, 11);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 23);
+            button1.TabIndex = 3;
+            button1.Text = "convert to XML";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(221, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 23);
+            button2.TabIndex = 4;
+            button2.Text = "convert to SQL";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(11, 84);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(532, 382);
+            textBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            button3.Enabled = false;
+            button3.Location = new Point(221, 41);
+            button3.Name = "button3";
+            button3.Size = new Size(107, 23);
+            button3.TabIndex = 7;
+            button3.Text = "Report";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Enabled = false;
+            button4.Location = new Point(109, 41);
+            button4.Name = "button4";
+            button4.Size = new Size(97, 23);
+            button4.TabIndex = 8;
+            button4.Text = "Report";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(556, 484);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(btnChoose);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Read Excel File";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnChoose;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ App/Form1.cs
@@ -0,0 +1,286 @@
+using Aspose.Words;
+using IronXL;
+using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Xml.XPath;
+
+namespace ReadExcelFileApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            createFolders();
+        }
+
+        OpenFileDialog file = new OpenFileDialog();
+        DataTable dtExcel;
+        String pathData = "./data";
+        String pathReport = "./report";
+        String dbName = "userdata";
+        String xmlName = "userdata";
+        String reportXML = "reportXML";
+        String reportSQL = "reportSQL";
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string fileExt = Path.GetExtension(file.FileName);
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        dtExcel = ReadExcel(file.FileName);
+                        button1.Enabled = true;
+                        button2.Enabled = true;
+                        button3.Enabled = true;
+                        button4.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private DataTable ReadExcel(string fileName)
+        {
+            WorkBook workbook = WorkBook.Load(fileName);
+            WorkSheet sheet = workbook.DefaultWorkSheet;
+            return sheet.ToDataTable(true);
+        }
+
+        private void createFolders()
+        {
+            if (!Directory.Exists(pathData))
+            {
+                Directory.CreateDirectory(pathData);
+            }
+            if (!Directory.Exists(pathReport))
+            {
+                Directory.CreateDirectory(pathReport);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dtExcel.WriteXml($"{pathData}/{xmlName}.xml", XmlWriteMode.IgnoreSchema);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            textBox1.AppendText("Файл xml успешно создан\r\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateDB(dbName);
+
+                // перебор всех строк таблицы
+                foreach (DataRow row in dtExcel.Rows)
+                {
+                    // получаем все ячейки строки
+                    var cells = row.ItemArray;
+                    String firstName = cells[0].ToString();
+                    String lastName = cells[1].ToString();
+                    String gender = cells[2].ToString();
+                    Double age = (double)cells[3];
+                    String status = cells[4].ToString();
+                    InsertData(dbName, firstName, lastName, gender, age, status);
+                }
+                textBox1.AppendText("База данных заполнена\r\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void CreateDB(string dbName)
+        {
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={pathData}/{dbName}.db"))
+                {
+                    connection.Open();
+
+                    SqliteCommand command = new SqliteCommand();
+                    command.Connection = connection;
+                    command.CommandText = "DROP TABLE IF EXISTS Users";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "CREATE TABLE Users(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, " +
+                        "firstName VARCHAR(50) NOT NULL, " +
+                        "lastName VARCHAR(50) NOT NULL, " +
+                        "gender VARCHAR(10) NOT NULL," +
+                        "age INTEGER NOT NULL," +
+                        "status VARCHAR(20) NOT NULL)";
+                    command.ExecuteNonQuery();
+
+                    textBox1.AppendText("Таблица Users создана\r\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        public void InsertData(String dbName, String firstName, String lastName, String gender, Double age, String status)
+        {
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={pathData}/{dbName}.db"))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO Users (firstName, lastName, gender, age, status) VALUES " +
+                                "(@firstName, @lastName, @gender, @age, @status)";
+
+                    using (SqliteCommand command = new SqliteCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@firstName", firstName);
+                        command.Parameters.AddWithValue("@lastName", lastName);
+                        command.Parameters.AddWithValue("@gender", gender);
+                        command.Parameters.AddWithValue("@age", age);
+                        command.Parameters.AddWithValue("@status", status);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={pathData}/{dbName}.db"))
+                {
+                    connection.Open();
+
+                    string sqlExpression = "SELECT COUNT(*) FROM Users WHERE gender='м'";
+                    SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                    object countMale = command.ExecuteScalar();
+
+                    command.CommandText = "SELECT COUNT(*) FROM Users WHERE gender='ж'";
+                    object countFemale = command.ExecuteScalar();
+
+                    command.CommandText = "SELECT COUNT(*) FROM Users WHERE (gender='м' AND age>30 AND age<40)";
+                    object countAgeMale = command.ExecuteScalar();
+
+
+                    command.CommandText = "SELECT COUNT(*) FROM Users WHERE status='стандарт'";
+                    object countStandart = command.ExecuteScalar();
+
+                    command.CommandText = "SELECT COUNT(*) FROM Users WHERE status='премиум'";
+                    object countPremium = command.ExecuteScalar();
+
+                    command.CommandText = "SELECT COUNT(*) FROM Users WHERE (gender='ж' AND status='премиум' AND age<30)";
+                    object femalePremiumAge = command.ExecuteScalar();
+
+                    textBox1.AppendText("Отчет:\r\n");
+                    textBox1.AppendText($"Мужчин: {countMale},\r\nЖенщин: {countFemale}\r\n");
+                    textBox1.AppendText($"Мужчин в возрасте 30-40 лет: {countAgeMale}\r\n");
+                    textBox1.AppendText($"Cтандартных: {countStandart}\r\nПремиум-аккаунтов: {countPremium}\r\n");
+                    textBox1.AppendText($"Женщин с премиум-аккаунтом в возрасте до 30 лет: {femalePremiumAge}.\r\n");
+
+
+                    Aspose.Words.Document doc = new Aspose.Words.Document();
+                    DocumentBuilder builder = new DocumentBuilder(doc);
+                    builder.Writeln("Отчет:");
+                    builder.Writeln($"Мужчин: {countMale},\r\nЖенщин: {countFemale},");
+                    builder.Writeln($"Мужчин в возрасте 30-40 лет: {countAgeMale},");
+                    builder.Writeln($"Cтандартных: {countStandart},\r\nПремиум-аккаунтов: {countPremium},");
+                    builder.Writeln($"Женщин с премиум-аккаунтом в возрасте до 30 лет: {femalePremiumAge}.");
+
+                    doc.Save($"{pathReport}/{reportSQL}.docx");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                XPathNavigator nav;
+                XPathDocument docNav;
+
+                // Open the XML.
+                docNav = new XPathDocument($"{pathData}/{xmlName}.xml");
+
+                // Create a navigator to query with XPath.
+                nav = docNav.CreateNavigator();
+
+                XPathNodeIterator male = nav.Select("/DocumentElement/Лист1/Пол[../Пол='м']");
+                XPathNodeIterator female = nav.Select("/DocumentElement/Лист1/Пол[../Пол='ж']");
+
+                // Получаем количество записей
+                int countMale = male.Count;
+                int countFemale = female.Count;
+
+                // Выводим количество записей на консоль
+                textBox1.AppendText("Отчет:\r\n");
+                textBox1.AppendText($"Количество мужчин: {countMale}\r\n");
+                textBox1.AppendText($"Количество женщин: {countFemale}\r\n");
+
+                XPathNodeIterator AgeMale = nav.Select("/DocumentElement/Лист1/Пол[../Пол='м' and ../Возраст>30 and ../Возраст<40]");
+
+                int countAgeMale = AgeMale.Count;
+
+                textBox1.AppendText($"Мужчин в возрасте 30-40 лет: {countAgeMale}\r\n");
+
+                XPathNodeIterator standart = nav.Select("/DocumentElement/Лист1/Статус_x0020_аккаунта[../Статус_x0020_аккаунта='стандарт']");
+                XPathNodeIterator premium = nav.Select("/DocumentElement/Лист1/Статус_x0020_аккаунта[../Статус_x0020_аккаунта='премиум']");
+
+                int countStandart = standart.Count;
+                int countPremium = premium.Count;
+
+                textBox1.AppendText($"Cтандартных: {countStandart}\r\nПремиум-аккаунтов: {countPremium}\r\n");
+
+                XPathNodeIterator femalePremiumAge = nav.Select("/DocumentElement/Лист1/Пол[../Пол='ж' and ../Статус_x0020_аккаунта='премиум' and ../Возраст<30]");
+
+                int countFemalePremiumAge = femalePremiumAge.Count;
+
+                textBox1.AppendText($"Женщин с премиум-аккаунтом в возрасте до 30 лет: {countFemalePremiumAge}.\r\n");
+
+
+                Aspose.Words.Document doc = new Aspose.Words.Document();
+                DocumentBuilder builder = new DocumentBuilder(doc);
+                builder.Writeln("Отчет:");
+                builder.Writeln($"Мужчин: {countMale},\r\nЖенщин: {countFemale},");
+                builder.Writeln($"Мужчин в возрасте 30-40 лет: {countAgeMale},");
+                builder.Writeln($"Cтандартных: {countStandart},\r\nПремиум-аккаунтов: {countPremium},");
+                builder.Writeln($"Женщин с премиум-аккаунтом в возрасте до 30 лет: {countFemalePremiumAge}.");
+
+                doc.Save($"{pathReport}/{reportXML}.docx");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
